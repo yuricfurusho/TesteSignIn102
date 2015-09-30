55f8a0e0d94697d4983f741bdd8ae79dcd12ad45
yuricfurusho <yuricfurusho@livetouch.com.br>
2015-09-30 14:02:47 -0300

    Format

--- app/src/main/java/yuricfurusho/testesignin102/MainActivity.java
+++ app/src/main/java/yuricfurusho/testesignin102/MainActivity.java
@@ -1,7 +1,7 @@
 package yuricfurusho.testesignin102;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
